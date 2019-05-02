11666f63b9a4db95e6e29c58726283ed6a389c77
jinyeong-jung <jinyeong32@gmail.com>
2019-05-02 14:34:12 +0900

    Deploying to Heroku

--- src/apollo.ts
+++ src/apollo.ts
@@ -8,6 +8,8 @@ import { WebSocketLink } from "apollo-link-ws";
 import { getMainDefinition } from "apollo-utilities";
 import { toast } from "react-toastify";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const cache = new InMemoryCache();
 
 const getToken = () => {
@@ -29,7 +31,9 @@ const authMiddleware = new ApolloLink((operation: Operation, forward: any) => {
 });
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql"
+  uri: isDev
+    ? "http://localhost:4000/graphql"
+    : "https://wethere.herokuapp.com/graphql"
 });
 
 const wsLink = new WebSocketLink({
@@ -39,7 +43,9 @@ const wsLink = new WebSocketLink({
     },
     reconnect: true
   },
-  uri: "ws://localhost:4000/subscription"
+  uri: isDev
+    ? "ws://localhost:4000/subscription"
+    : "wss://wethere.herokuapp.com/subscription"
 });
 
 const combinedLinks = split(
